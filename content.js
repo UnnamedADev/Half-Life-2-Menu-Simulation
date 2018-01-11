c9fee33fef67977a0393f3e04a8bf6fcccd1f549
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-01-11 19:58:28 +0100

    v0.1.7
    JAVASCRIPT
    + In content.js changed "new game" dialog window content, extactle h3 has been exchanged for p tag
    CSS
    + Created dialog window's content style

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
     var content = [];
 
     // new game
-    content["new game"] = "<hr> <table> <tbody> <tr><td><h3>Chapter 1<br>point insertion</h3></td><td><h3>Chapter 2<br>a red letter day</h3></td><td><h3>Chapter 3<br>route kanal</h3></td></tr>    <tr><td><img src='images/chapters/chapter1.png'></td><td><img src='images/chapters/chapter2.png'></td><td><img src='images/chapters/chapter3.png'></td></tr>    </tbody>    </table>    <button>More&#62;</button><hr><button>Start new game</button><button>Cancel</button>";
+    content["new game"] = "<hr> <table> <tbody> <tr><td><p>Chapter 1<br>point insertion</p></td><td><p>Chapter 2<br>a red letter day</p></td><td><p>Chapter 3<br>route kanal</p></td></tr>    <tr><td><img src='images/chapters/chapter1.png'></td><td><img src='images/chapters/chapter2.png'></td><td><img src='images/chapters/chapter3.png'></td></tr>    </tbody>    </table>    <button>More &#62;</button><hr><button>Start new game</button><button>Cancel</button>";
 
     // load game
     content["load game"] = "";
