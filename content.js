b814d3e405c696d659acf51ad4c4309e55a34c8f
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-01-12 01:15:39 +0100

    v0.1.9
    JAVASCRIPT
    + In content.js modified "new game" window's content
    + In content.js modified "load game" window's content
    CSS
    + Added global style for in game options buttons
    + Added style for many elements of "load game" window

--- content.js
+++ content.js
@@ -2,10 +2,10 @@
     var content = [];
 
     // new game
-    content["new game"] = "<hr> <table> <tbody> <tr><td><p>Chapter 1<br>point insertion</p></td><td><p>Chapter 2<br>a red letter day</p></td><td><p>Chapter 3<br>route kanal</p></td></tr>    <tr><td><img src='images/chapters/chapter1.png'></td><td><img src='images/chapters/chapter2.png'></td><td><img src='images/chapters/chapter3.png'></td></tr>    </tbody>    </table>    <button>More &#62;</button><hr><button>Start new game</button><button>Cancel</button>";
+    content["new game"] = "<hr> <table> <tbody> <tr><td><p>Chapter 1<br>point insertion</p></td><td><p>Chapter 2<br>a red letter day</p></td><td><p>Chapter 3<br>route kanal</p></td></tr>    <tr><td><img src='images/chapters/chapter1.png'></td><td><img src='images/chapters/chapter2.png'></td><td><img src='images/chapters/chapter3.png'></td></tr>    </tbody>    </table>    <button class='steam_button'>More &#62;</button><hr><button class='steam_button active_button'>Start new game</button><button class='steam_button'>Cancel</button>";
 
     // load game
-    content["load game"] = "";
+    content["load game"] = "<p>Select a saved game in the list belo, then click 'Load game'.</p><div>                                                         <table><tbody><tr><td><img src='images/chapters/chapter1.png'></td><td><h3>water hazard</h3><p>Fri Jul 21 14:51:57 2017</p></td></tr><tr><td><img src='images/chapters/chapter1.png'></td><td><h3>water hazard</h3><p>Fri Jul 21 14:51:57 2017</p></td></tr><tr><td><img src='images/chapters/chapter1.png'></td><td><h3>water hazard</h3><p>Fri Jul 21 14:51:57 2017</p></td></tr><tr><td><img src='images/chapters/chapter1.png'></td><td><h3>water hazard</h3><p>Fri Jul 21 14:51:57 2017</p></td></tr><tr><td><img src='images/chapters/chapter1.png'></td><td><h3>water hazard</h3><p>Fri Jul 21 14:51:57 2017</p></td></tr><tr><td><img src='images/chapters/chapter1.png'></td><td><h3>water hazard</h3><p>Fri Jul 21 14:51:57 2017</p></td></tr></tbody></table>                                                                                 </div><button class='steam_button'>Delete</button><button class='steam_button'>Load</button><button class='steam_button'>Cancel</button>";
 
     // achievements
     content["achievements"] = "";
